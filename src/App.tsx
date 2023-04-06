4a5a1fad53bd136c8a4c6e09a854f65a34015d0c
MasahikoYonezawa <y.masahiko0802@gmail.com>
2023-04-06 18:18:27 +0900

    feat: AutoBatchOther.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {AutoBatchEventHandler} from "./components/AutoBatchEventHandler";
+import {AutoBatchOther} from "./components/AutoBatchOther";
 
 function App() {
   console.log("Appがレンダリング！")
   return (
     <div className="App">
-      <AutoBatchEventHandler />
+      <AutoBatchEventHandler /><AutoBatchOther/>
     </div>
   );
 }
